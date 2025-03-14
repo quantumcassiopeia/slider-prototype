4315ee989c8b17e2b26a71c8e3d26b200f316a9c
Quantum <russewelt@gmail.com>
2025-03-13 21:49:58 -0300

    slider implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const arrayImagens = [
+    {
+      url: "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
+      alt: "Imagem 1",
+      link: "#",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      alt: "Imagem 2",
+      link: "#",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/14/2500/1667.jpg?hmac=ssQyTcZRRumHXVbQAVlXTx-MGBxm6NHWD3SryQ48G-o",
+      alt: "Imagem 3",
+      link: "#",
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % arrayImagens.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [arrayImagens.length]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % arrayImagens.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + arrayImagens.length) % arrayImagens.length
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="slider">
+        <div className="arrows">
+          <span
+            className="material-icons-outlined arrowLeft"
+            onClick={prevSlide}
+          >
+            expand_circle_down
+          </span>
+          <span
+            className="material-icons-outlined arrowRight"
+            onClick={nextSlide}
+          >
+            expand_circle_down
+          </span>
+        </div>
+
+        <a href={arrayImagens[currentSlide].link}>
+          <img
+            className="imagemSlider"
+            src={arrayImagens[currentSlide].url}
+            alt={arrayImagens[currentSlide].alt}
+          />
         </a>
-      </header>
+
+        <div className="dots">
+          {arrayImagens.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === currentSlide ? "active" : ""}`}
+              onClick={() => setCurrentSlide(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
